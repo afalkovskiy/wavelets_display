6160c5ceed58da95bb98bd3146fcfed3fa301ce3
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-27 22:56:34 -0600

    Update 03_klauder.py

--- pages/03_klauder.py
+++ pages/03_klauder.py
@@ -21,14 +21,13 @@ def ORMSBY(f1=5., f2=10., f3=40., f4=45., length=0.512, dt=0.001):
 
     return t, y
 
-def Klauder(f1=5., f2=10., f3=40., f4=45., length=0.512, dt=0.001):
+def Klauder(T=6., f1=10., f2=40., length=0.512, dt=0.001):
     p = np.pi
     t = np.linspace(-length/2, (length-dt)/2, int(length/dt))
+    y = t**2
 
-    # y = p*p*f4**2 * (np.sinc(f4*t))**2/(p*f4-p*f3) - p*p*f3**2 * (np.sinc(f3*t))**2/(p*f4-p*f3) - \
-    #     p*p*f2**2 * (np.sinc(f2*t))**2/(p*f2-p*f1) - p*p*f1**2 * (np.sinc(f1*t))**2/(p*f2-p*f1)
-    y = p*f4**2 * (np.sinc(f4*t))**2/(f4-f3) - p*f3**2 * (np.sinc(f3*t))**2/(f4-f3) - \
-        p*f2**2 * (np.sinc(f2*t))**2/(f2-f1) - p*f1**2 * (np.sinc(f1*t))**2/(f2-f1)
+    #y = p*f4**2 * (np.sinc(f4*t))**2/(f4-f3) - p*f3**2 * (np.sinc(f3*t))**2/(f4-f3) - \
+    #    p*f2**2 * (np.sinc(f2*t))**2/(f2-f1) - p*f1**2 * (np.sinc(f1*t))**2/(f2-f1)
 
     y = y / np.amax(abs(y))
 
@@ -45,7 +44,7 @@ f1 = 5
 f2 = 10
 f3 = f
 f4 = f+20.
-t, y = Klauder(f1, f2, f3, f4, 0.512, 0.001)
+t, y = Klauder(7, f1, f2, 0.512, 0.001)
 
 chart_data = pd.DataFrame(
    {
