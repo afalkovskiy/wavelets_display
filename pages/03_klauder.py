3f02580718418f87c0561ba2a17eba9ddc9d1cfb
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-27 22:30:37 -0600

    Update 03_klauder.py

--- pages/03_klauder.py
+++ pages/03_klauder.py
@@ -1,4 +1,43 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+#from scipy.signal import hilbert, chirp
+import math
+
+pi = math.pi
+
+def ORMSBY(f1=5., f2=10., f3=40., f4=45., length=0.512, dt=0.001):
+    p = np.pi
+    t = np.linspace(-length/2, (length-dt)/2, int(length/dt))
+
+    # y = p*p*f4**2 * (np.sinc(f4*t))**2/(p*f4-p*f3) - p*p*f3**2 * (np.sinc(f3*t))**2/(p*f4-p*f3) - \
+    #     p*p*f2**2 * (np.sinc(f2*t))**2/(p*f2-p*f1) - p*p*f1**2 * (np.sinc(f1*t))**2/(p*f2-p*f1)
+    y = p*f4**2 * (np.sinc(f4*t))**2/(f4-f3) - p*f3**2 * (np.sinc(f3*t))**2/(f4-f3) - \
+        p*f2**2 * (np.sinc(f2*t))**2/(f2-f1) - p*f1**2 * (np.sinc(f1*t))**2/(f2-f1)
+
+    y = y / np.amax(abs(y))
+
+    return t, y
 
 st.title('Klauder wavelet')
 st.text('This is a web app do display wavelets')
+
+st.subheader("f(t) = ...")
+f = st.slider('Select frequency from [1, 240] Hz', value=60., min_value=1., max_value=240.)
+st.write("Frequency f3 = ", f)
+
+f1 = 5
+f2 = 10
+f3 = f
+f4 = f+20.
+t, y = ORMSBY(f1, f2, f3, f4, 0.512, 0.001)
+
+chart_data = pd.DataFrame(
+   {
+       "t": t,
+       "y": y
+   }
+)
+
+st.line_chart(chart_data, x="t", y="y")
