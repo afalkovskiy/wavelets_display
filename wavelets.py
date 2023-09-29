53b618e21fe281afb36cffefa6c488c05fd548e3
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-28 22:26:14 -0600

    Update wavelets.py

--- wavelets.py
+++ wavelets.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Seismic wavelets')
 st.header('by Alex Falkovskiy')
-st.text('The purpose of this web app is to illustrate basic seismic wavelet and how their shape changes with the change of parameters.')
+st.write('The purpose of this web app is to illustrate basic seismic wavelet and how their shape changes with the change of parameters.')
