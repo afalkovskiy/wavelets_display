85162b6d1dbb7a77cf5dd37ccfe758aee7b5737e
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-28 23:54:00 -0600

    Update 03_klauder.py

--- pages/03_klauder.py
+++ pages/03_klauder.py
@@ -45,7 +45,7 @@ st.text('This is a web app do display wavelets')
 #st.subheader("f(t) = ...")
 st.latex(r'''
     real [sin(\pi kt(T-t))/(\pi kt) exp (2 \pi ifot)]
-
+    , where k = (f2 - fl)/T, fo = (f2 + fl)/2, i = \sqrt{-1}
     ''')
 st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
