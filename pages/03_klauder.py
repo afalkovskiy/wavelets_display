c25bc6ae6a277dcde1f1488d88dd8bfd782ab0d7
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-29 15:49:23 -0600

    Update 03_klauder.py

--- pages/03_klauder.py
+++ pages/03_klauder.py
@@ -53,9 +53,10 @@ st.latex(r'''
   #  \sum_{k=0}^{n-1} ar^k =
  #   a \left(\frac{1-r^{n}}{1-r}\right)
  #   ''')
-
-f1 = st.slider('Select terminal low frequency (Hz)', value=10., min_value=1., max_value=240.)
-f2 = st.slider('Select terminal high frequency (Hz)', value=40., min_value=f1, max_value=240.)
+f1=10.
+f2=40.
+f1 = st.slider('Select terminal low frequency (Hz)', min_value=1., max_value=240.)
+f2 = st.slider('Select terminal high frequency (Hz)', min_value=f1, max_value=240.)
 
 st.write(f1, " - ", f2)
 
