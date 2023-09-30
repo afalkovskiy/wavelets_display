2368ac71462402f7df4788525fcc7b07d83ac578
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-29 18:35:28 -0600

    Update 02_ormsby.py

--- pages/02_ormsby.py
+++ pages/02_ormsby.py
@@ -25,8 +25,8 @@ st.text('This is a web app do display wavelets')
 
 
 st.latex(r'''
-    Ormsby(t) = \frac{\pi^2f_4^2sinc^2 (\pi f_4 t)}{\pi f_4 - \pi f_3}  
-    - \frac{\pi^2f_3^2}{\pi f_4 - \pi f_3} sinc^2 (\pi f_3 t)
+    Ormsby(t) = \frac{\pi^2f_4^2 sinc^2 (\pi f_4 t)}{\pi f_4 - \pi f_3}  
+    - \frac{\pi^2f_3^2 sinc^2 (\pi f_3 t)}{\pi f_4 - \pi f_3} 
     - \frac{\pi^2f_2^2}{\pi f_2 - \pi f_1} sinc^2 (\pi f_4 t)
     + \frac{\pi^2f_1^2}{\pi f_2 - \pi f_1} sinc^2 (\pi f_4 t)
     ''') 
