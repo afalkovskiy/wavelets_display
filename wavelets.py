948d2927aeca5e325f69bc3fff1467163e6e7458
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-29 20:44:34 -0600

    Update wavelets.py

--- wavelets.py
+++ wavelets.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.title('Seismic Wavelets')
-st.header('Alex Falkovskiy')
+st.subheader('Alex Falkovskiy')
+url = "https://rmseismic.com"
+st.write("Alex Falkovskiy, RM Seismic Software [rmseismic.com](%s)" % url)
 st.write('The purpose of this web app is to illustrate basic seismic wavelet and how their shape changes with the change of parameters.')
 st.write('It is based on a paper by Harold Ryan: Ricker, Ormsby; Klander, Bntterwo - A Choice of avelets publised in CSEG Recorder, September 1994.')
