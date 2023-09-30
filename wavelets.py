88884ba08a2ae7c70a319aaaea22c44505843f7a
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-29 21:54:26 -0600

    Update wavelets.py

--- wavelets.py
+++ wavelets.py
@@ -7,7 +7,7 @@ st.write("RM Seismic Software [rmseismic.com](%s)" % url)
 st.write('The purpose of this web app is to illustrate how basic seismic wavelets change with the change of parameters.')
 st.write('It is based on a paper by Harold Ryan: Ricker, Ormsby; Klander, Bntterworth - A Choice of wavelets, publised in CSEG Recorder, September 1994.')
 st.write('A list of wavelets you can display and vary parameters:')
-st.write('Ricker, Ormsby, Klauder - use left side menu to select.')
+st.write('**Ricker, Ormsby, Klauder** - use left side menu to select.')
 
 url = "https://www.rmseismic.com/lasviewer.html"
 st.write("More geophysical web apps: [rmseismic.com/lasviewer.html](%s)" % url)
