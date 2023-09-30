80484abc02eb7fbf691440fd6fb2bf3de3b9a1ca
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-29 22:06:18 -0600

    Update 02_ormsby.py

--- pages/02_ormsby.py
+++ pages/02_ormsby.py
@@ -60,3 +60,6 @@ chart_data = pd.DataFrame(
 )
 
 st.line_chart(chart_data, x="t", y="y")
+
+url1 = "https://www.rmseismic.com/lasviewer.html"
+st.write("More geophysical web apps: [link](%s)" % url1)
